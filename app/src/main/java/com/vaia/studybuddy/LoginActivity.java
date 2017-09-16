900efd7e479dd839e8d8c375a326958547cb0de5
Ivan Mondragon <imondrag@umich.edu>
2017-09-16 08:05:13 -0400

    Added app bar activity

--- app/src/main/java/com/vaia/studybuddy/LoginActivity.java
+++ app/src/main/java/com/vaia/studybuddy/LoginActivity.java
@@ -3,6 +3,7 @@ package com.vaia.studybuddy;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,7 +334,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                //intent.putExtra();
+                startActivity(intent);
+                //finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
